8756fde3b51d40e71ffe9c80cc7cafeb2de0c648
Pavin Wu <pw.skywalk@gmail.com>
2017-07-24 18:07:30 +1000

    Solution to problem 2 (Try to implement merge_sort!)

--- 2.py
+++ 2.py
@@ -1,7 +1,39 @@
 """
 Solution to challange 2:
 """
+## Attempting to have the key,value automatically sorted by using Maxheap
+# class KV():
+	# def __init__(self, key, val):
+		# self.key = key;
+		# self.val = val;
+		
+# class Maxheap():
+	# def __init__(self, key, val):
+		# self.head = KV
+		# self.head.key = key
+		# self.head.val = val
+		
+		# self.left = None
+		# self.right = None
+	
+	# def traverseDown(self):
+		# prevHead = self.head
+		
+		
+		# return prevHead
+		
+	# def getMax(self):
+		# if(self.head is None):
+			# return None
+		# else:
+			 
+		
+	# def insert(self, key, val):
 
+# ... just use built-in solution
+
+
+######
 #srcFile = open("2_ocr.html", "r")
 # https://stackoverflow.com/questions/24755022/why-html2text-module-throws-unicodedecodeerror
 # https://stackoverflow.com/questions/28610508/how-to-get-raw-html-text-of-a-given-url-using-python
@@ -12,16 +44,29 @@ url = "http://www.pythonchallenge.com/pc/def/ocr.html"
 html = urllib.request.urlopen(url)
 
 flag = False
+line_count = 0
+dict = {}
 
 for line in html:
-	# print (line)
-	if (line[:2] == "%%"):
-		flag = True
-	
-	if (flag):
-		for char in line:
-			if (( char >='A' and char <= 'Z') or (char >= 'a' and char <= 'z') 
-					or (ordCh in [' ', '\t', '\n'])):
-				print (char, end="")
+	#print (line)
+	if (line_count > 37):	# skip non-mess parts (by inspection)
+		for ordCh in line:
+			if ordCh not in dict:
+				dict[ordCh] = [1, line_count]	# #occurance, the line which it occurred
+			else:
+				dict[ordCh][0] += 1				# update freq
+				dict[ordCh][1] = line_count		# update line
+	line_count += 1
 
-srcFile.close()
+keyList = []
+for key, val in dict.items():
+	if val[0] == 1:		# know that "rare" means having exactly one occurance (by inspection)
+		keyList.append([val[1],chr(key)])
+keyList.sort()	# sort by first element
+print(keyList)
+
+## Solution: rare characters are
+## 	a, l, -, q, t, u, i, y, >
+## sorted: equality
+
+
